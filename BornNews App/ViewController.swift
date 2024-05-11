d81cf0515eb85fda2c11f1303058db86c138c893
Lucas Migge <53379393+miggelucas@users.noreply.github.com>
2024-05-11 13:59:45 -0300

    first commit

---
+++ BornNews App/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  BornNews App
+//
+//  Created by Lucas Migge on 11/05/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
